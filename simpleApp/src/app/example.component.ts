ca46d18bcc6801d58ac6fa588486956bf58712bf
Alexander Wolf <wolf.alexander.development@gmail.com>
2024-09-05 00:00:27 +0200

    Create simpleApp with one component manually

---
+++ simpleApp/src/app/example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'example',
+    template: '<p>This is the example component!</p>'
+})
+export class ExampleComponent {
+    
+}
