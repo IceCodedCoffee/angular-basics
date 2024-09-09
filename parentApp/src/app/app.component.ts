a441d410db0ffb7a5b887e204add22a997ce4d14
Alexander Wolf <wolf.alexander.development@gmail.com>
2024-09-09 21:34:49 +0200

    Create parent-child components with data binding

---
+++ parentApp/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Book } from './book.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  cart: number = 0;
+  readonly STOCK: number = 5;
+  books: Book[] = [
+    {
+      title: 'HTML and CSS Book',
+      imagePath: 'book.jpg'
+    }
+  ]
+
+  inStock(): boolean {
+    return this.cart < this.STOCK;
+  }
+
+  outOfStock(): boolean {
+    return this.cart >= this.STOCK;
+  }
+
+  getRemainingStock(): number {
+    return this.STOCK - this.cart;
+  }
+
+  addToCart(): void {
+    this.cart++;
+  }
+
+  removeFromCart(): void {
+    this.cart > 0 ? this.cart-- : null;
+  }
+}
---
+++ parentApp/src/app/child/child.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Book } from '../book.interface';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss'
+})
+export class ChildComponent {
+    @Input('book') book!: Book;
+    @Input('cart') cart!: number;
+    @Input('available') inStock!: boolean;
+    @Input('notAvailable') outOfStock!: boolean;
+    @Input('remainingStock') getRemainingStock!: number;
+    @Output() readonly addToCartEvent: EventEmitter<Book> = new EventEmitter();
+    @Output() readonly removeFromCartEvent: EventEmitter<Book> = new EventEmitter();
+
+    emitAddEvent(): void {
+      this.addToCartEvent.emit(this.book);
+    }
+
+    emitRemoveEvent(): void {
+      this.removeFromCartEvent.emit(this.book);
+    }
+}
