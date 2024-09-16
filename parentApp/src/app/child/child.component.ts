bdf341b0ea566284304c7ab94b14636e11e36bd2
Alexander Wolf <wolf.alexander.development@gmail.com>
2024-09-16 22:29:23 +0200

    Implement several lifecycle hooks

--- parentApp/src/app/child/child.component.ts
+++ parentApp/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges, DoCheck, OnInit } from '@angular/core';
 import { Book } from '../book.interface';
 
 @Component({
@@ -13,20 +13,38 @@ import { Book } from '../book.interface';
     `
   ]
 })
-export class ChildComponent {
-    @Input('book') book!: Book;
-    @Input('cart') cart!: number;
-    @Input('available') inStock!: boolean;
-    @Input('notAvailable') outOfStock!: boolean;
-    @Input('remainingStock') getRemainingStock!: number;
-    @Output() readonly addToCartEvent: EventEmitter<Book> = new EventEmitter();
-    @Output() readonly removeFromCartEvent: EventEmitter<Book> = new EventEmitter();
+export class ChildComponent implements OnChanges, OnInit, DoCheck {
+  @Input('book') book!: Book;
+  @Input('cart') cart!: number;
+  @Input('available') inStock!: boolean;
+  @Input('notAvailable') outOfStock!: boolean;
+  @Input('remainingStock') getRemainingStock!: number;
+  @Output() readonly addToCartEvent: EventEmitter<Book> = new EventEmitter();
+  @Output() readonly removeFromCartEvent: EventEmitter<Book> = new EventEmitter();
 
-    emitAddEvent(): void {
-      this.addToCartEvent.emit(this.book);
-    }
+  private oldValue: number = 0;
 
-    emitRemoveEvent(): void {
-      this.removeFromCartEvent.emit(this.book);
-    }
+  emitAddEvent(): void {
+    this.addToCartEvent.emit(this.book);
+  }
+
+  emitRemoveEvent(): void {
+    this.removeFromCartEvent.emit(this.book);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges: ", changes)
+    console.log("ngOnChanges previousValue: ", changes['getRemainingStock'].previousValue);
+    console.log("ngOnChanges currentValue: ", changes['getRemainingStock'].currentValue);
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit : ", this.getRemainingStock);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck : (old value)", this.oldValue);
+    console.log("ngDoCheck : (new value)", this.getRemainingStock);
+    this.oldValue = this.getRemainingStock;
+  }
 }
