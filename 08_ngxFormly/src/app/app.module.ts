a67858d543aeb2ac14cd8cb7f3cc3a8c3220005d
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-10-05 14:27:31 +0200

    Implement ngxFormly

---
+++ 08_ngxFormly/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import {FormlyPrimeNGModule} from '@ngx-formly/primeng';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyPrimeNGModule,
+    ButtonModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
